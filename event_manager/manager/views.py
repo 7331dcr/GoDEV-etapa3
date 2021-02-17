0809871b8744a620dc1bb93891cb8b48e2c2a3c7
rcd1337 <cs50hypers@gmail.com>
2021-02-17 03:27:35 -0300

    added views

--- event_manager/manager/views.py
+++ event_manager/manager/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "manager/index.html")
+
+def cadastro(request):
+    return render(request, "manager/cadastro.html")
+
+def consulta(request):
+    return render(request, "manager/consulta.html")
