e1ac0c79c8667f86652cf6ec4dc35a2c8683507f
rcd1337 <cs50hypers@gmail.com>
2021-02-18 10:59:57 -0300

    updated urls

--- event_manager/manager/urls.py
+++ event_manager/manager/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path ("", views.index, name="index"),
     path("cadastro", views.cadastro, name="cadastro"),
+    path("cadastro_sala", views.cadastro_sala, name="cadastro_sala"),
+    path("cadastro_cafe", views.cadastro_cafe, name="cadastro_cafe"),
     path("consulta", views.consulta, name="consulta")
 ] 
--- event_manager/manager/views.py
+++ event_manager/manager/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+testlist = ['Teste 1', 'Teste 2', 'Teste 3', 'Teste 4']
 
 def index(request):
     return render(request, "manager/index.html")
@@ -7,5 +8,13 @@ def index(request):
 def cadastro(request):
     return render(request, "manager/cadastro.html")
 
+def cadastro_sala(request):
+    return render(request, "manager/cadastro_sala.html")
+
+def cadastro_cafe(request):
+    return render(request, "manager/cadastro_cafe.html")
+
 def consulta(request):
-    return render(request, "manager/consulta.html")
+    return render(request, "manager/consulta.html", {
+        "entries": testlist
+    })
