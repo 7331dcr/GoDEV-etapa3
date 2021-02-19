022cf718a7de823c0e6688829f01d04ef0056958
rcd1337 <cs50hypers@gmail.com>
2021-02-19 14:18:27 -0300

    created database

--- event_manager/manager/models.py
+++ event_manager/manager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Event_room(models.Model):
+    name = models.TextField(blank=False)
+    capacity = models.IntegerField(blank=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Coffee_space(models.Model):
+    name = models.TextField(blank=False)
+    capacity = models.IntegerField(blank=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Attendee(models.Model):
+    name = models.TextField(blank=False)
+    last_name = models.TextField(blank=False)
+    event_room = models.ForeignKey("Event_room")
+    timestamp = models.DateTimeField(auto_now_add=True)
