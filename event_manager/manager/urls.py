ed2ce758af1540da797feab33161ac593c624077
rcd1337 <cs50hypers@gmail.com>
2021-02-21 23:04:50 -0300

    updated urls

--- event_manager/manager/urls.py
+++ event_manager/manager/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
     path ("", views.index, name="index"),
     path("cadastro", views.cadastro, name="cadastro"),
-    path("cadastro_sala", views.cadastro_sala, name="cadastro_sala"),
+    path("pre_cadastro_sala", views.pre_cadastro_sala, name="pre_cadastro_sala"),
+    path("cadastro_sala/<str:form_quantity>", views.cadastro_sala, name="cadastro_sala"),
     path("cadastro_cafe", views.cadastro_cafe, name="cadastro_cafe"),
-    path("consulta", views.consulta, name="consulta")
+    path("consulta", views.consulta, name="consulta"),
+    path("consulta_sala", views.consulta_sala, name="consulta_sala"),
+    path("consulta_cafe", views.consulta_cafe, name="consulta_cafe")
 ] 
